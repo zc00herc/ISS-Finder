5150e389ff164c4a08a80b47f78564f1fbae3e28
zc00herc <96848546+zc00herc@users.noreply.github.com>
2024-10-10 11:18:58 -0700

    renamed main to practice to build new main

---
+++ practice.py
@@ -0,0 +1,32 @@
+import requests
+from datetime import datetime
+
+MY_LAT = 49.234669
+MY_LNG = -124.807587
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json()
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+#
+# iss_position = (longitude,latitude)
+#
+# print(iss_position)
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LNG,
+    "formatted": 0
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+time_now = datetime.now().hour
+if sunset <= time_now <= sunrise:
+    print(time_now)
+    print(f"sunrise: {sunrise}, sunset: {sunset}")
